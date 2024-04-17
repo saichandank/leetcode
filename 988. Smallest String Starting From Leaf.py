1fefdd59b70edbacf4cf2cb6d5dec27bbf000937
saichandank <36816095+saichandank@users.noreply.github.com>
2024-04-17 17:42:59 -0400

    Create 988. Smallest String Starting From Leaf

---
+++ 988. Smallest String Starting From Leaf.py
@@ -0,0 +1,21 @@
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        self.smallest_string = ""
+        self.dfs(root, "")
+        return self.smallest_string
+
+    def dfs(self, root, current_string):
+        if not root:
+            return
+            
+        current_string = chr(root.val + ord('a')) + current_string
+
+        if not root.left and not root.right:
+            if not self.smallest_string or self.smallest_string > current_string:
+                self.smallest_string = current_string
+
+        if root.left:
+            self.dfs(root.left, current_string)
+        
+        if root.right:
+            self.dfs(root.right, current_string)
